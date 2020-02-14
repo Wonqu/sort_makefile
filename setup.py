50c15c23a6c11fd96f9baa420b65036999a3d96f
Radosław Zachar <radoslaw.zachar@stxnext.pl>
2020-02-14 13:00:10 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup
+
+setup(
+      name='sort_makefile',
+      version='0.4',
+      description='Sort makefile.',
+      url='http://github.com/Wonqu/sort_makefile',
+      author='Radosław Zachar',
+      license='MIT',
+      packages=['sort_makefile'],
+      zip_safe=False
+)
+
